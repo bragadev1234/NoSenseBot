94b4e3fbce16eaf164512d9a793324158f09d1a9
Braga <204622249+braga2311@users.noreply.github.com>
2025-07-23 04:21:44 -0300

    Criando comando de casamento baseado nos outros

---
+++ src/commands/member/funny/casar.js
@@ -0,0 +1,49 @@
+const { PREFIX } = require(`${BASE_DIR}/config`);
+const { InvalidParameterError } = require(`${BASE_DIR}/errors`);
+const { toUserJid, onlyNumbers } = require(`${BASE_DIR}/utils`);
+const path = require("node:path");
+const { ASSETS_DIR } = require(`${BASE_DIR}/config`);
+
+module.exports = {
+  name: "tapa",
+  description: "Se casa com alguem",
+  commands: ["casar"],
+  usage: `${PREFIX}casar @usuario`,
+  /**
+   * @param {CommandHandleProps} props
+   * @returns {Promise<void>}
+   */
+  handle: async ({
+    sendGifFromFile,
+    sendErrorReply,
+    userJid,
+    replyJid,
+    args,
+    isReply,
+  }) => {
+    if (!args.length && !isReply) {
+      throw new InvalidParameterError(
+        "Você precisa mencionar ou marcar um membro!"
+      );
+    }
+
+    const targetJid = isReply ? replyJid : toUserJid(args[0]);
+
+    if (!targetJid) {
+      await sendErrorReply(
+        "Você precisa mencionar um usuário ou responder uma mensagem para se casar"
+      );
+
+      return;
+    }
+
+    const userNumber = onlyNumbers(userJid);
+    const targetNumber = onlyNumbers(targetJid);
+
+    await sendGifFromFile(
+      path.resolve(ASSETS_DIR, "images", "funny", "casar.mp4"),
+      `@${userNumber} Acabou de se casar com @${targetNumber}!  🔥`,
+      [userJid, targetJid]
+    );
+  },
+};
