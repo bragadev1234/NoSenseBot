6251c3d64aa115b0639312e402e6d17947e36439
ProfaneReaper <204622249+ProfaneReaper@users.noreply.github.com>
2025-08-17 17:35:33 -0300

    Update menu.js

--- src/menu.js
+++ src/menu.js
@@ -5,69 +5,70 @@ exports.menuMessage = (senderName) => {
   const date = new Date();
 
   return `
-🌟━━━━━━━━━━━━━━━━━━🌟  
-       *MENU DO ${BOT_NAME}*  
-🌟━━━━━━━━━━━━━━━━━━🌟  
+✨━━━━━━━━━━━━━━━━━━✨  
+        *MENU DO ${BOT_NAME}*  
+✨━━━━━━━━━━━━━━━━━━✨  
 
-🔷 *ADMINISTRAÇÃO* 🔷  
-1. ${PREFIX}abrir/fechar 🚪  
-2. ${PREFIX}ban 🔨  
-3. ${PREFIX}promover/rebaixar 🎖️  
-4. ${PREFIX}limpar 🧹  
-5. ${PREFIX}anti-link 🔗🚫  
-6. ${PREFIX}anti-audio 🔇🚫  
-7. ${PREFIX}anti-sticker 🖼️🚫  
-8. ${PREFIX}anti-video 🎥🚫  
-9. ${PREFIX}anti-document 📄🚫  
-10. ${PREFIX}welcome 🎉  
-11. ${PREFIX}exit 👋  
-12. ${PREFIX}marcartodos 📢  
-13. ${PREFIX}agendar-mensagem ⏰  
+🛡️ *ADMINISTRAÇÃO* 🛡️  
+🚪 *${PREFIX}abrir/fechar*  
+🔨 *${PREFIX}ban*  
+🎖️ *${PREFIX}promover/rebaixar*  
+🧹 *${PREFIX}limpar*  
+🔗🚫 *${PREFIX}anti-link*  
+🔇🚫 *${PREFIX}anti-audio*  
+🖼️🚫 *${PREFIX}anti-sticker*  
+🎥🚫 *${PREFIX}anti-video*  
+📄🚫 *${PREFIX}anti-document*  
+🎉 *${PREFIX}welcome*  
+👋 *${PREFIX}exit*  
+📢 *${PREFIX}marcartodos*  
+⏰ *${PREFIX}agendar-mensagem*  
+📜 *${PREFIX}regras*  
 
-🔹 *INFORMAÇÕES* 🔹  
-14. ${PREFIX}cep 🪪  
-15. ${PREFIX}ip 📄  
-16. ${PREFIX}loctel 🧾  
-17. ${PREFIX}cnpj 💳  
-18. ${PREFIX}placa 🗳️  
-19. ${PREFIX}bin 🧑‍🎓  
-20. ${PREFIX}cpf 📍  
-21. ${PREFIX}ddd 📞  
-22. ${PREFIX}perfil 🙋  
-23. ${PREFIX}signododia ♈  
-24. ${PREFIX}ascendentedodia 🌌  
+🔎 *INFORMAÇÕES* 🔎  
+🪪 *${PREFIX}cep*  
+📄 *${PREFIX}ip*  
+🧾 *${PREFIX}loctel*  
+💳 *${PREFIX}cnpj*  
+🗳️ *${PREFIX}placa*  
+🧑‍🎓 *${PREFIX}bin*  
+📍 *${PREFIX}cpf*  
+📞 *${PREFIX}ddd*  
+🙋 *${PREFIX}perfil*  
+♈ *${PREFIX}signododia*  
+🌌 *${PREFIX}ascendentedodia*  
 
-🔹 *MÍDIA & ARQUIVOS* 🔹  
-25. ${PREFIX}gerar-link 🌐  
-26. ${PREFIX}revelar 👁️  
-27. ${PREFIX}to-image 🖼️  
-28. ${PREFIX}video 🎥  
-29. ${PREFIX}tocar 🎵  
-30. ${PREFIX}ytsearch ▶️  
-31. ${PREFIX}google 🔎  
-32. ${PREFIX}resumir-pdf 📑✂️  
-33. ${PREFIX}resumir-texto 📝✂️  
+🎨 *MÍDIA & ARQUIVOS* 🎨  
+🌐 *${PREFIX}gerar-link*  
+👁️ *${PREFIX}revelar*  
+🖼️ *${PREFIX}to-image*  
+🖌️ *${PREFIX}gerar-imagem*  
+🔎 *${PREFIX}google-led*  
+📝✂️ *${PREFIX}resumir*  
+💡 *Dica*: No comando *${PREFIX}s* você pode escolher um dos filtros: blur, grayscale, sepia, invert, cartoon, pixelate, vintage, emboss, glow, sketch, flip, mirror, rotate, negate, contrast  
 
-🔹 *DIVERSÃO & INTERAÇÃO* 🔹  
-34. ${PREFIX}casar 💍  
-35. ${PREFIX}cassanic 🎰  
-36. ${PREFIX}lutar ⚔️  
-37. ${PREFIX}matar/socar 🩸  
-38. ${PREFIX}dado 🎲  
-39. ${PREFIX}beijar/abracar 💋🤗  
-40. ${PREFIX}torta 🥧  
-41. ${PREFIX}caracoroa 🪙  
-42. ${PREFIX}tapa 👋  
-43. ${PREFIX}jantar 🍽️  
+🎲 *DIVERSÃO & INTERAÇÃO* 🎲  
+💍 *${PREFIX}casar*  
+🎰 *${PREFIX}cassanic*  
+⚔️ *${PREFIX}lutar*  
+🩸 *${PREFIX}matar/socar*  
+🎲 *${PREFIX}dado*  
+💋🤗 *${PREFIX}beijar/abracar*  
+🥧 *${PREFIX}torta*  
+🪙 *${PREFIX}caracoroa*  
+👋 *${PREFIX}tapa*  
+🍽️ *${PREFIX}jantar*  
 
-🔹 *UTILIDADES* 🔹  
-44. ${PREFIX}ping 📶  
+⚙️ *UTILIDADES* ⚙️  
+📶 *${PREFIX}ping*  
+🌐 *${PREFIX}traduzir*  
+💼 *${PREFIX}vagas30*  
 
-📅 Data: ${date.toLocaleDateString()}  
-⏰ Hora: ${date.toLocaleTimeString()}  
+📅 *Data:* ${date.toLocaleDateString()}  
+⏰ *Hora:* ${date.toLocaleTimeString()}  
 
-🌟━━━━━━━━━━━━━━━━━━🌟  
-       *${BOT_NAME} v${packageInfo.version}*  
-🌟━━━━━━━━━━━━━━━━━━🌟  
+✨━━━━━━━━━━━━━━━━━━✨  
+        *${BOT_NAME} v${packageInfo.version}*  
+✨━━━━━━━━━━━━━━━━━━✨  
 `;
 };
