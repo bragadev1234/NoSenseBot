88c16b556ce24fd72a0adc7c1f68516e49b39103
Braga Dev <204622249+bragadev1234@users.noreply.github.com>
2025-09-17 22:28:05 -0300

    Update menu.js

--- src/menu.js
+++ src/menu.js
@@ -5,120 +5,120 @@ exports.menuMessage = (senderName) => {
   const date = new Date();
 
   return `
-✨━━━━━━━━━━━━━ MENU ━━━━━━━━━━━━━✨  
-👋 Olá, *${senderName}*!  
-Aqui estão os comandos disponíveis no *${BOT_NAME}* 🤖  
+✨━━━━━━━━━━━━━ 𝑴𝑬𝑵𝑼 ━━━━━━━━━━━━━✨  
+👋 𝑶𝑙𝒂́, *${senderName}*!  
+𝑨𝒒𝒖𝒊 𝒆𝒔𝒕𝒂̃𝒐 𝒐𝒔 𝒄𝒐𝒎𝒂𝒏𝒅𝒐𝒔 𝒅𝒊𝒔𝒑𝒐𝒏𝒊́𝒗𝒆𝒊𝒔 𝒏𝒐 *${BOT_NAME}* 🤖  
 
-📅 *Data:* ${date.toLocaleDateString()}  
-⚙️ *Versão:* v${packageInfo.version}  
-
-━━━━━━━━━━━━━━━━━━━━━━━━━━
-🔧 *ADMINISTRAÇÃO*  
-🚪 ${PREFIX}abrir / ${PREFIX}fechar  
-🔨 ${PREFIX}ban  
-🎖️ ${PREFIX}promover / ${PREFIX}rebaixar  
-🧹 ${PREFIX}limpar  
-📢 ${PREFIX}marcartodos  
-🎉 ${PREFIX}welcome  
-👋 ${PREFIX}exit  
-💾 ${PREFIX}infogrupo | linkgrupo | listaadm  
+📅 *𝑫𝒂𝒕𝒂:* ${date.toLocaleDateString()}  
+⚙️ *𝑽𝒆𝒓𝒔𝒂̃𝒐:* v${packageInfo.version}  
 
 ━━━━━━━━━━━━━━━━━━━━━━━━━━
-🚫 *ANTIS*  
-🔗 ${PREFIX}anti-link  
-🔇 ${PREFIX}anti-audio  
-🖼️ ${PREFIX}anti-sticker  
-🎥 ${PREFIX}anti-video  
-📄 ${PREFIX}anti-document  
+🔧 *𝑨𝑫𝑴𝑰𝑵𝑰𝑺𝑻𝑹𝑨𝑪̧𝑨̃𝑶*  
+🚪 ${PREFIX}𝒂𝒃𝒓𝒊𝒓 / ${PREFIX}𝒇𝒆𝒄𝒉𝒂𝒓  
+🔨 ${PREFIX}𝒃𝒂𝒏  
+🎖️ ${PREFIX}𝒑𝒓𝒐𝒎𝒐𝒗𝒆𝒓 / ${PREFIX}𝒓𝒆𝒃𝒂𝒊𝒙𝒂𝒓  
+🧹 ${PREFIX}𝒍𝒊𝒎𝒑𝒂𝒓  
+📢 ${PREFIX}𝒎𝒂𝒓𝒄𝒂𝒓𝒕𝒐𝒅𝒐𝒔  
+🎉 ${PREFIX}𝒘𝒆𝒍𝒄𝒐𝒎𝒆  
+👋 ${PREFIX}𝒆𝒙𝒊𝒕  
+💾 ${PREFIX}𝒊𝒏𝒇𝒐𝒈𝒓𝒖𝒑𝒐 | 𝒍𝒊𝒏𝒌𝒈𝒓𝒖𝒑𝒐 | 𝒍𝒊𝒔𝒕𝒂𝒂𝒅𝒎  
 
 ━━━━━━━━━━━━━━━━━━━━━━━━━━
-📚 *UTILITÁRIOS*  
-📜 ${PREFIX}regras  
-🪪 ${PREFIX}cep  
-📄 ${PREFIX}ip  
-🚏 ${PREFIX}qr  
-🧾 ${PREFIX}loctel  
-💳 ${PREFIX}cnpj  
-📞 ${PREFIX}tel3  
-📞 ${PREFIX}ddd  
-🗳️ ${PREFIX}placa  
-🧑‍🎓 ${PREFIX}bin  
-📍 ${PREFIX}cpf  
-🙋 ${PREFIX}perfil  
-🗣️ ${PREFIX}ig  
-🎫 ${PREFIX}email  
-🧐 ${PREFIX}geo  
-🗃️ ${PREFIX}numero2  
-🌐 ${PREFIX}gerar-link  
-👁️ ${PREFIX}revelar  
-🖼️ ${PREFIX}to-image  
-🖌️ ${PREFIX}gerar-imagem  
-📶 ${PREFIX}ping  
-🚨 ${PREFIX}gerarsenha  
-🗓️ ${PREFIX}calculadora  
-🌐 ${PREFIX}traduzir  
-💼 ${PREFIX}vagas30  
-🇵🇹 ${PREFIX}nif  
-🇪🇸 ${PREFIX}espanhola  
-🔎 ${PREFIX}google-led  
-📝✂️ ${PREFIX}resumir  
-🤔 ${PREFIX}wiki  
+🚫 *𝑨𝑵𝑻𝑰𝑺*  
+🔗 ${PREFIX}𝒂𝒏𝒕𝒊-𝒍𝒊𝒏𝒌  
+🔇 ${PREFIX}𝒂𝒏𝒕𝒊-𝒂𝒖𝒅𝒊𝒐  
+🖼️ ${PREFIX}𝒂𝒏𝒕𝒊-𝒔𝒕𝒊𝒄𝒌𝒆𝒓  
+🎥 ${PREFIX}𝒂𝒏𝒕𝒊-𝒗𝒊𝒅𝒆𝒐  
+📄 ${PREFIX}𝒂𝒏𝒕𝒊-𝒅𝒐𝒄𝒖𝒎𝒆𝒏𝒕  
 
 ━━━━━━━━━━━━━━━━━━━━━━━━━━
-🎭 *DIVERSÃO & JOGOS*  
-🎰 ${PREFIX}cassanic  
-⚔️ ${PREFIX}lutar  
-🩸 ${PREFIX}matar / ${PREFIX}socar  
-🎲 ${PREFIX}dado  
-💋🤗 ${PREFIX}beijar / ${PREFIX}abracar  
-🥧 ${PREFIX}torta  
-🪙 ${PREFIX}caracoroa  
-👋 ${PREFIX}tapa  
-🍽️ ${PREFIX}jantar  
-🏪 ${PREFIX}get-lid  
+📚 *𝑼𝑻𝑰𝑳𝑰𝑻𝑨́𝑹𝑰𝑶𝑺*  
+📜 ${PREFIX}𝒓𝒆𝒈𝒓𝒂𝒔  
+🪪 ${PREFIX}𝒄𝒆𝒑  
+📄 ${PREFIX}𝒊𝒑  
+🚏 ${PREFIX}𝒒𝒓  
+🧾 ${PREFIX}𝒍𝒐𝒄𝒕𝒆𝒍  
+💳 ${PREFIX}𝒄𝒏𝒑𝒋  
+📞 ${PREFIX}𝒕𝒆𝒍3  
+📞 ${PREFIX}𝒅𝒅𝒅  
+🗳️ ${PREFIX}𝒑𝒍𝒂𝒄𝒂  
+🧑‍🎓 ${PREFIX}𝒃𝒊𝒏  
+📍 ${PREFIX}𝒄𝒑𝒇  
+🙋 ${PREFIX}𝒑𝒆𝒓𝒇𝒊𝒍  
+🗣️ ${PREFIX}𝒊𝒈  
+🎫 ${PREFIX}𝒆𝒎𝒂𝒊𝒍  
+🧐 ${PREFIX}𝒈𝒆𝒐  
+🗃️ ${PREFIX}𝒏𝒖𝒎𝒆𝒓𝒐2  
+🌐 ${PREFIX}𝒈𝒆𝒓𝒂𝒓-𝒍𝒊𝒏𝒌  
+👁️ ${PREFIX}𝒓𝒆𝒗𝒆𝒍𝒂𝒓  
+🖼️ ${PREFIX}𝒕𝒐-𝒊𝒎𝒂𝒈𝒆  
+🖌️ ${PREFIX}𝒈𝒆𝒓𝒂𝒓-𝒊𝒎𝒂𝒈𝒆𝒎  
+📶 ${PREFIX}𝒑𝒊𝒏𝒈  
+🚨 ${PREFIX}𝒈𝒆𝒓𝒂𝒓𝒔𝒆𝒏𝒉𝒂  
+🗓️ ${PREFIX}𝒄𝒂𝒍𝒄𝒖𝒍𝒂𝒅𝒐𝒓𝒂  
+🌐 ${PREFIX}𝒕𝒓𝒂𝒅𝒖𝒛𝒊𝒓  
+💼 ${PREFIX}𝒗𝒂𝒈𝒂𝒔30  
+🇵🇹 ${PREFIX}𝒏𝒊𝒇  
+🇪🇸 ${PREFIX}𝒆𝒔𝒑𝒂𝒏𝒉𝒐𝒍𝒂  
+🔎 ${PREFIX}𝒈𝒐𝒐𝒈𝒍𝒆-𝒍𝒆𝒅  
+📝✂️ ${PREFIX}𝒓𝒆𝒔𝒖𝒎𝒊𝒓  
+🤔 ${PREFIX}𝒘𝒊𝒌𝒊  
 
 ━━━━━━━━━━━━━━━━━━━━━━━━━━
-📖 *CULTURA & ANIME*  
-🇯🇵 ${PREFIX}anime  
-🎵 ${PREFIX}letra  
-♈ ${PREFIX}signododia  
-🌌 ${PREFIX}ascendentedodia  
+🎭 *𝑫𝑰𝑽𝑬𝑹𝑺𝑨̃𝑶 & 𝑱𝑶𝑮𝑶𝑺*  
+🎰 ${PREFIX}𝒄𝒂𝒔𝒔𝒂𝒏𝒊𝒄  
+⚔️ ${PREFIX}𝒍𝒖𝒕𝒂𝒓  
+🩸 ${PREFIX}𝒎𝒂𝒕𝒂𝒓 / ${PREFIX}𝒔𝒐𝒄𝒂𝒓  
+🎲 ${PREFIX}𝒅𝒂𝒅𝒐  
+💋🤗 ${PREFIX}𝒃𝒆𝒊𝒋𝒂𝒓 / ${PREFIX}𝒂𝒃𝒓𝒂𝒄𝒂𝒓  
+🥧 ${PREFIX}𝒕𝒐𝒓𝒕𝒂  
+🪙 ${PREFIX}𝒄𝒂𝒓𝒂𝒄𝒐𝒓𝒐𝒂  
+👋 ${PREFIX}𝒕𝒂𝒑𝒂  
+🍽️ ${PREFIX}𝒋𝒂𝒏𝒕𝒂𝒓  
+🏪 ${PREFIX}𝒈𝒆𝒕-𝒍𝒊𝒅  
 
 ━━━━━━━━━━━━━━━━━━━━━━━━━━
-🐾 *PETS*  
-🐶 ${PREFIX}pet  
-📋 ${PREFIX}meuspets  
-🏆 ${PREFIX}pet rank  
-ℹ️ ${PREFIX}pet info  
+📖 *𝑪𝑼𝑳𝑻𝑼𝑹𝑨 & 𝑨𝑵𝑰𝑴𝑬*  
+🇯🇵 ${PREFIX}𝒂𝒏𝒊𝒎𝒆  
+🎵 ${PREFIX}𝒍𝒆𝒕𝒓𝒂  
+♈ ${PREFIX}𝒔𝒊𝒈𝒏𝒐𝒅𝒐𝒅𝒊𝒂  
+🌌 ${PREFIX}𝒂𝒔𝒄𝒆𝒏𝒅𝒆𝒏𝒕𝒆𝒅𝒐𝒅𝒊𝒂  
 
 ━━━━━━━━━━━━━━━━━━━━━━━━━━
-🔥 *NSFW +18*  
-🤤 ${PREFIX}hentai  
-💦 ${PREFIX}sexo1 / ${PREFIX}sexo2 / ${PREFIX}sexo3  
-🚶 ${PREFIX}sexoempe  
-🍑 ${PREFIX}sentar2  
-💧 ${PREFIX}sexomolhado  
-👄 ${PREFIX}boquete  
-😋 ${PREFIX}chuparpeitos  
-🎌 ${PREFIX}sexohegal  
-👭 ${PREFIX}sexo-lesbica  
-💋 ${PREFIX}beijo-lesbico  
-🌸 ${PREFIX}sexo-yuri  
-✋ ${PREFIX}apalpar-amiga  
-🤲 ${PREFIX}apalpar  
-🍆 ${PREFIX}sexo-futa1 / ${PREFIX}sexo-futa2  
-💅 ${PREFIX}sexo-femboy  
-✌️ ${PREFIX}siririca / ${PREFIX}siririca2  
-🍈🍈 ${PREFIX}mostrarospeitos  
-👅 ${PREFIX}chuparbct  
-👀 ${PREFIX}sexo-loli  
+🐾 *𝑷𝑬𝑻𝑺*  
+🐶 ${PREFIX}𝒑𝒆𝒕  
+📋 ${PREFIX}𝒎𝒆𝒖𝒔𝒑𝒆𝒕𝒔  
+🏆 ${PREFIX}𝒑𝒆𝒕 𝒓𝒂𝒏𝒌  
+ℹ️ ${PREFIX}𝒑𝒆𝒕 𝒊𝒏𝒇𝒐  
 
 ━━━━━━━━━━━━━━━━━━━━━━━━━━
-😂 *RANKS MEMES*  
-🤡 ${PREFIX}rank-corno  
-🥊 ${PREFIX}rank-jabateu  
-🎲 ${PREFIX}rank-jadeu  
-👅 ${PREFIX}rank-mamada  
+🔥 *𝑵𝑺𝑭𝑾 +18*  
+🤤 ${PREFIX}𝒉𝒆𝒏𝒕𝒂𝒊  
+💦 ${PREFIX}𝒔𝒆𝒙𝒐1 / ${PREFIX}𝒔𝒆𝒙𝒐2 / ${PREFIX}𝒔𝒆𝒙𝒐3  
+🚶 ${PREFIX}𝒔𝒆𝒙𝒐𝒆𝒎𝒑𝒆  
+🍑 ${PREFIX}𝒔𝒆𝒏𝒕𝒂𝒓2  
+💧 ${PREFIX}𝒔𝒆𝒙𝒐𝒎𝒐𝒍𝒉𝒂𝒅𝒐  
+👄 ${PREFIX}𝒃𝒐𝒒𝒖𝒆𝒕𝒆  
+😋 ${PREFIX}𝒄𝒉𝒖𝒑𝒂𝒓𝒑𝒆𝒊𝒕𝒐𝒔  
+🎌 ${PREFIX}𝒔𝒆𝒙𝒐𝒉𝒆𝒈𝒂𝒍  
+👭 ${PREFIX}𝒔𝒆𝒙𝒐-𝒍𝒆𝒔𝒃𝒊𝒄𝒂  
+💋 ${PREFIX}𝒃𝒆𝒊𝒋𝒐-𝒍𝒆𝒔𝒃𝒊𝒄𝒐  
+🌸 ${PREFIX}𝒔𝒆𝒙𝒐-𝒚𝒖𝒓𝒊  
+✋ ${PREFIX}𝒂𝒑𝒂𝒍𝒑𝒂𝒓-𝒂𝒎𝒊𝒈𝒂  
+🤲 ${PREFIX}𝒂𝒑𝒂𝒍𝒑𝒂𝒓  
+🍆 ${PREFIX}𝒔𝒆𝒙𝒐-𝒇𝒖𝒕𝒂1 / ${PREFIX}𝒔𝒆𝒙𝒐-𝒇𝒖𝒕𝒂2  
+💅 ${PREFIX}𝒔𝒆𝒙𝒐-𝒇𝒆𝒎𝒃𝒐𝒚  
+✌️ ${PREFIX}𝒔𝒊𝒓𝒊𝒓𝒊𝒄𝒂 / ${PREFIX}𝒔𝒊𝒓𝒊𝒓𝒊𝒄𝒂2  
+🍈🍈 ${PREFIX}𝒎𝒐𝒔𝒕𝒓𝒂𝒓𝒐𝒔𝒑𝒆𝒊𝒕𝒐𝒔  
+👅 ${PREFIX}𝒄𝒉𝒖𝒑𝒂𝒓𝒃𝒄𝒕  
+👀 ${PREFIX}𝒔𝒆𝒙𝒐-𝒍𝒐𝒍𝒊  
+
+━━━━━━━━━━━━━━━━━━━━━━━━━━
+😂 *𝑹𝑨𝑵𝑲𝑺 𝑴𝑬𝑴𝑬𝑺*  
+🤡 ${PREFIX}𝒓𝒂𝒏𝒌-𝒄𝒐𝒓𝒏𝒐  
+🥊 ${PREFIX}𝒓𝒂𝒏𝒌-𝒋𝒂𝒃𝒂𝒕𝒆𝒖  
+🎲 ${PREFIX}𝒓𝒂𝒏𝒌-𝒋𝒂𝒅𝒆𝒖  
+👅 ${PREFIX}𝒓𝒂𝒏𝒌-𝒎𝒂𝒎𝒂𝒅𝒂  
 
 ✨━━━━━━━━━━━━━━━━━━✨  
 `;
